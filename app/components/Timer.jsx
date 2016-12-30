41a6d5f28e3d1b0be9f304498cd510d0d2e56726
Kimmo Halonen <Kimmo Halonen>
2016-12-30 15:38:41 +0200

    Timer component added

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,9 +1,76 @@
 var React = require('react');
+var Clock = require('Clock');
+var CountdownForm = require('CountdownForm');
+var Controls = require('Controls');
 
 var Timer = React.createClass({
+  getInitialState: function () {
+    return {
+      count:0,
+      countdownStatus: 'paused'
+    };
+  },
+
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({count: 0, countdownStatus: 'paused'});
+        case 'paused':
+          clearInterval(this.timer);
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+
+  componentWillUnmount: function () {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
+
+  startTimer: function () {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count + 1;
+      this.setState({
+        count: newCount
+      });
+//      if (newCount === 0) {
+//        this.setState({countdownStatus: 'stopped'});
+//      }
+    }, 1000);
+  },
+
+  handleSetCountdown: function (seconds) {
+    this.setState ({
+      count: seconds,
+      countdownStatus: 'paused'
+    });
+  },
+
+  handleStatusChange: function (newStatus) {
+    this.setState({countdownStatus: newStatus});
+  },
+
   render: function () {
+    var {count, countdownStatus} = this.state;
+    var renderControlArea = () => {
+//      if (countdownStatus !== 'stopped') {
+        return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>;
+//      } else {
+//        return <CountdownForm onSetCountdown={this.handleSetCountdown}/>;
+//      }
+    }
+
     return (
-      <p>timer.jsx</p>
+      <div>
+        <h1 className="page-title">Timer</h1>
+        <Clock totalSeconds={count}/>
+        {renderControlArea()}
+      </div>
     )
   }
 });
